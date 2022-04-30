860f66e240b3378c39b0b86cd909e49bc273620f
Ajsa99 <ajsa.alibasic11@gmail.com>
2022-04-30 20:01:20 +0200

    new

---
+++ HTML/class28/index.js
@@ -0,0 +1,50 @@
+// const obj = {user: 'Ajsa', 
+// age:23, 
+// address: {street: 'Kolubarska', number: 60, city: "Novi Pazar"},
+// };
+
+// const {user, age, address} = obj;
+
+// console.log(user, age, address);
+
+// const {user, age, address} = obj;
+// const {street, number, city} = address;
+
+// console.log(street);
+
+
+// const[a, b, c, d] = [1, 2, 3, 4, 5];
+
+// console.log(a, b, c, d);
+
+// const array = [1, 2, 3, 4, 5];
+// const[x, , , y] = array;
+
+// console.log(x,y); 
+
+
+
+// class Car{
+//     constructor(type, price){
+//         this.type = type;
+//         this.price = price;
+//     }
+//     getPrice(){
+//         return this.price;
+//     }
+//     getType(){
+//         return this.type;
+//     }
+//     toString(){
+//         return JSON.stringify(this);
+//     }
+// }
+
+// const car1 = new Car('bmw', 20000);
+// const car2 = new Car('mercedes', 23000);
+
+// console.log(car1);
+
+// console.log(car1.toString());
+
+
